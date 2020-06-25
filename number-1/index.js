4973c51a8498aeabcb7fb02a55d12dd676c726c8
Aharon Brownstein <aybrownstein@gmail.com>
2020-06-25 10:56:24 -0400

    little tweak

--- number-1/index.js
+++ number-1/index.js
@@ -3,7 +3,15 @@
 function getDogImage(num) {
     fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson));
+        .then(responseJson => displayResults(responseJson));
+}
+
+function displayResults(responseJson) {
+
+    responseJson.message.forEach((url) => {
+        console.log(url)
+
+    });
 }
 
 
